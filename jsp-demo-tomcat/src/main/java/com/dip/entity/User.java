e16a3cdc3280073ddfb063991af4568d135a3cbd
moneg <monegask1969@gmail.com>
2017-01-20 00:26:14 +0200

    Не работает: регаистрация/авторизация Попытка на регистрацию/авторизацию поиск Исправленно: изменена таблица собак

--- jsp-demo-tomcat/src/main/java/com/dip/entity/User.java
+++ jsp-demo-tomcat/src/main/java/com/dip/entity/User.java
@@ -1,6 +1,11 @@
 package com.dip.entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.Set;
 
 /**
@@ -8,7 +13,7 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements Serializable, UserDetails {
 
     private int id;
     private String username;
@@ -32,6 +37,11 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 
     public String getPassword() {
@@ -59,5 +69,25 @@ public class User {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
